fac5a466a81a11733150e3b6a880af71d3047ee6
someone <something@apa.com>
2019-07-05 10:57:31 -0400

    Add project files.

---
+++ CheckoutSystemKataTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CheckoutSystemKataTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
